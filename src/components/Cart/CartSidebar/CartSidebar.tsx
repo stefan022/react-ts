193759744009b5e6329e46733a99b35c37b957ef
stefan022 <stefan022github@gmail.com>
2023-07-27 15:04:15 +0200

    refactor CartSidebar and add CartSidebarContent

--- src/components/Cart/CartSidebar/CartSidebar.tsx
+++ src/components/Cart/CartSidebar/CartSidebar.tsx
@@ -3,13 +3,13 @@ import CartSidebarContent from '../CartSidebarContent/CartSidebarContent';
 
 interface IProps {
     displayCart: boolean;
-    handleHideCart: MouseEventHandler<HTMLAnchorElement>
+    handleHideCart: MouseEventHandler<HTMLElement>
 }
 
 const CartSidebar = ({ displayCart, handleHideCart }: IProps) => {
     return (
         <div
-            className={`${displayCart ? "w-[350px]" : "w-0"} fixed top-0 right-0 h-screen bg-blue-400 transition-all duration-500 z-30`}>
+            className={`${displayCart ? "w-[350px]" : "w-0"} fixed top-0 right-0 h-screen bg-white transition-all duration-500 z-30`}>
             { displayCart ? <CartSidebarContent handleHideCart={handleHideCart}/> : null }
         </div>
     )
--- src/components/Cart/CartSidebarContent/CartSidebarContent.tsx
+++ src/components/Cart/CartSidebarContent/CartSidebarContent.tsx
@@ -2,15 +2,62 @@ import React, { MouseEventHandler } from 'react'
 import { Link } from 'react-router-dom'
 import { Routes } from '../../../router/Routes'
 
+import { MdOutlineCancel } from "react-icons/md";
+
 interface IProps {
-    handleHideCart: MouseEventHandler<HTMLAnchorElement>
+    handleHideCart: MouseEventHandler<HTMLElement> 
 }
 
 const CartSidebarContent = ({ handleHideCart }: IProps) => {
     return (
         <div>
-            <p>Cart:</p>
-            <Link onClick={handleHideCart} to={Routes.CART}>View Cart</Link>
+            <div className="flex items-center justify-between p-4 border border-b-gray-200">
+                <h3 className="font-bold text-xl">My Cart</h3>
+                <div onClick={handleHideCart} className='cursor-pointer rounded-full'><MdOutlineCancel color="gray" size={26}/></div>
+            </div>
+            <div className="flex p-4 border border-b-gray-200">
+                <div className="w-1/4">
+                    <div className="bg-blue-300 w-[75px] h-[100px]"></div>
+                </div>
+                <div className='flex flex-col justify-between ml-2 w-2/4'>
+                    <div>
+                        <p>Article Name</p>
+                        <p>$price | o status</p>
+                    </div>
+                    <div className="border border-gray-100 flex items-center justify-between p-1 w-3/4">
+                        <div className="bg-gray-100 w-3/12 text-center">-</div>
+                        <div className="w-6/12 text-center">1</div>
+                        <div className="bg-gray-100 w-3/12 text-center">+</div>
+                    </div>
+                </div>
+                <div className="ml-2 flex flex-col justify-between w-1/4">
+                    <div className='flex justify-end'>
+                    <MdOutlineCancel color="gray" size={22}/>
+                    </div>
+                    <div className='flex justify-end'>$totalPrice</div>
+                </div>
+            </div>
+            <div className='p-4 border border-b-gray-200'>
+                <div className='flex justify-between mb-4'>
+                    <p>Shipping Charge :</p>
+                    <p>$shipingChargePrice</p>
+                </div>
+                <div className='flex justify-between'>
+                    <p>Estimated Tax (12.5%) :</p>
+                    <p>$eslimitedTaxPrice</p>
+                </div>
+            </div>
+            <div className='p-4'>
+                <div className='flex justify-between mb-4'>
+                    <p>Total Amount:</p>
+                    <p>$totalAmountPrice</p>
+                </div>
+                <div className='flex'>
+                    <Link onClick={handleHideCart} to={Routes.CART}>
+                        <button className="bg-gray-200 py-1 px-3">View Cart</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
