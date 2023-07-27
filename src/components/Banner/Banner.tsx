93811c6f883966f907a54e97f77a7989600eceeb
stefan022 <stefan022github@gmail.com>
2023-07-27 12:54:34 +0200

    change background style for Banner component

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
+import logo from "../../assets/logo.png";
+
 interface IProps {
     title: string;
 }
 
 const Banner = ({ title }: IProps) => {
     return (
-        <div className='bg-blue-400 h-[150px] w-full flex items-center justify-center'>
+        <div
+            style={{ backgroundImage: `linear-gradient(90deg, rgba(45,139,255,.7) 40%, rgba(96,165,250,.7) 80%)` }} 
+            className=' h-[200px] w-full flex items-center justify-center'>
             <h2 className="text-2xl text-white">{title}</h2>
         </div>
     )
