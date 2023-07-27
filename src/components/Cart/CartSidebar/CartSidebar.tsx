38e638478a18dcd1cdc21e6db2b821ccc906ac72
stefan022 <stefan022github@gmail.com>
2023-07-27 16:57:55 +0200

    change CartSidebar logic

--- src/components/Cart/CartSidebar/CartSidebar.tsx
+++ src/components/Cart/CartSidebar/CartSidebar.tsx
@@ -9,7 +9,7 @@ interface IProps {
 const CartSidebar = ({ displayCart, handleHideCart }: IProps) => {
     return (
         <div
-            className={`${displayCart ? "w-[350px]" : "w-0"} fixed top-0 right-0 h-screen bg-white transition-all duration-500 z-30`}>
+            className={`${displayCart ? "right-0" : "-right-full"} w-[400px] fixed top-0 h-screen bg-white transition-all duration-500 z-30`}>
             { displayCart ? <CartSidebarContent handleHideCart={handleHideCart}/> : null }
         </div>
     )
