ef024db1a6385b59cc6d6a5f67616fa20c564eac
stefan022 <stefan022github@gmail.com>
2023-07-28 23:02:18 +0200

    add content of Settings in Profile page

--- src/pages/Profile/Settings/Settings.tsx
+++ src/pages/Profile/Settings/Settings.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const Settings = () => {
-  return (
-    <div>Settings</div>
-  )
+    return (
+        <div>
+            <div className='p-4 flex justify-between border border-gray-300'>
+                <p className='font-bold'>Settings</p>
+            </div>
+            <div className='p-4 border border-gray-300 flex flex-col justify-between'>
+                <div className='flex justify-between'>
+                    <p>Notification</p>
+                    <div>$toggle</div>
+                </div>
+                <div className='flex justify-between'>
+                    <p>Messages</p>
+                    <div>$toggle</div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Settings
