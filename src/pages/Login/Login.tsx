5970ead38e81cc04630dd281c9d158212bd3bd83
stefan022 <stefan022github@gmail.com>
2023-08-01 13:28:19 +0200

    change wallpaper for Login and Register page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import wallpaper from "../../assets/wallpaper.jpg"
+import wallpaper from "../../assets/wallpaper.png"
 import logo from "../../assets/logo.png"
 import SignIn from '../../components/Forms/SignIn/SignIn'
 
@@ -9,7 +9,7 @@ const Login = () => {
         <div className="flex items-center">
             <div className="relative w-1/2">
                 <img className="absolute top-5 right-0 z-10" src={logo} alt="logo.png" />
-                <img className="w-full h-screen relative" src={wallpaper} alt="wallpaper.jpg"/>
+                <img className="w-full h-screen relative" src={wallpaper} alt="wallpaper.png"/>
             </div>
             <div className="w-1/2">
                 <SignIn/>
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import SignUp from "../../components/Forms/SignUp/SignUp";
 
 import logo from "../../assets/logo.png";
-import wallpaper from "../../assets/wallpaper.jpg";
+import wallpaper from "../../assets/wallpaper.png";
 
 const Register = () => {
 	return (
 		<div className="flex items-center">
 			<div className="relative w-1/2">
                 <img className="absolute top-5 right-0 z-10" src={logo} alt="logo.png" />
-                <img className="w-full h-screen relative" src={wallpaper} alt="wallpaper.jpg"/>
+                <img className="w-full h-screen relative" src={wallpaper} alt="wallpaper.png"/>
             </div>
 			<div className="w-1/2">
 				<SignUp />
