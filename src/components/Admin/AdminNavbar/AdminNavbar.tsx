5b41ef882e706f9d83118f43f147e0c618660a8f
stefan022 <stefan022github@gmail.com>
2023-08-02 11:02:49 +0200

    add content of AdminNavbar

--- src/components/Admin/AdminNavbar/AdminNavbar.tsx
+++ src/components/Admin/AdminNavbar/AdminNavbar.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const AdminNavbar = () => {
-  return (
-    <div>AdminNavbar</div>
-  )
+    return (
+        <div className='py-5 px-4 flex justify-end items-center gap-4 border border-gray-400'>
+            <div>$toggleTheme</div>
+            <div>$notification</div>
+            <div className='border border-gray-400 w-10 h-10 rounded-full'></div>
+        </div>
+    )
 }
 
 export default AdminNavbar
