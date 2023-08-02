b44ef97c46f98317253ac8798a42734620801a20
stefan022 <stefan022github@gmail.com>
2023-08-02 16:40:55 +0200

    update AdminMessages page

--- src/pages/Admin/AdminMessages/AdminMessages.tsx
+++ src/pages/Admin/AdminMessages/AdminMessages.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from "react";
 
 const AdminMessages = () => {
-  return (
-    <div>AdminMessages</div>
-  )
-}
+	return (
+		<div>
+			<div className="flex justify-center py-4 bg-gray-200">
+				<h3>Messages</h3>
+			</div>
 
-export default AdminMessages
+			<div className="flex flex-col border border-gray-400">
+                <div className="flex flex-col p-4">
+                    <div className="flex justify-between items-center">
+                        <h3>$username</h3>
+                        <button className="text-blue-400">View</button>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, aut?</p>
+                </div>
+            </div>
+			<div className="flex flex-col border border-gray-400">
+                <div className="flex flex-col p-4">
+                    <div className="flex justify-between items-center">
+                        <h3>$username</h3>
+                        <button className="text-blue-400">View</button>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, aut?</p>
+                </div>
+            </div>
+			<div className="flex flex-col border border-gray-400">
+                <div className="flex flex-col p-4">
+                    <div className="flex justify-between items-center">
+                        <h3>$username</h3>
+                        <button className="text-blue-400">View</button>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, aut?</p>
+                </div>
+            </div>
+			<div className="flex flex-col border border-gray-400">
+                <div className="flex flex-col p-4">
+                    <div className="flex justify-between items-center">
+                        <h3>$username</h3>
+                        <button className="text-blue-400">View</button>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, aut?</p>
+                </div>
+            </div>
+			<div className="flex flex-col border border-gray-400">
+                <div className="flex flex-col p-4">
+                    <div className="flex justify-between items-center">
+                        <h3>$username</h3>
+                        <button className="text-blue-400">View</button>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, aut?</p>
+                </div>
+            </div>
+			<div className="flex flex-col border border-gray-400">
+                <div className="flex flex-col p-4">
+                    <div className="flex justify-between items-center">
+                        <h3>$username</h3>
+                        <button className="text-blue-400">View</button>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, aut?</p>
+                </div>
+            </div>
+
+            {/* After click view open modal with Title and Message */}
+            {/* If Admin not send answer, available write in answer */}
+            {/* otherwise answer is available readonly!!! */}
+		</div>
+	);
+};
+
+export default AdminMessages;
