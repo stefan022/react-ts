2d1e2ca321f38e1343bbe3ee1eadeee988f992e6
stefan022 <stefan022github@gmail.com>
2023-08-08 22:43:33 +0200

    change CartOverlay component

--- src/components/Cart/CartOverlay/CartOverlay.tsx
+++ src/components/Cart/CartOverlay/CartOverlay.tsx
@@ -9,7 +9,7 @@ const CartOverlay = ({ displayCart, handleHideCart }: IProps) => {
     return (
         <div 
 			onClick={handleHideCart}
-			className={`${displayCart ? "opacity-50" : "opacity-0 hidden"} fixed top-0 left-0 w-full h-screen bg-black transition-all duration-500 z-20`}>
+			className={`${displayCart ? "opacity-90" : "opacity-0 hidden"} fixed top-0 left-0 w-full h-screen bg-white transition-all duration-500 z-20`}>
         </div>
     )
 }
