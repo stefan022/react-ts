ace21f6d589a0deba0c46b7a2b78017729fad596
stefan022 <stefan022github@gmail.com>
2023-08-08 20:00:29 +0200

    refactor ResetPassword page

--- src/components/Forms/Auth/AuthResetPassword/AuthResetPassword.tsx
+++ src/components/Forms/Auth/AuthResetPassword/AuthResetPassword.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useRef } from 'react'
+import React, { FC, Dispatch, SetStateAction, FormEvent, useRef } from 'react'
 import { AuthField, AuthForm, AuthFormButton, AuthFormSwitch, AuthFormTitle, AuthOr } from '../../../../components'
 import { sendPasswordResetEmail } from 'firebase/auth';
 import { auth } from '../../../../firebase/config';
@@ -6,21 +6,28 @@ import { toast } from 'react-toastify';
 import { Routes } from '../../../../router/Routes';
 import { useNavigate } from 'react-router-dom';
 
-export const AuthResetPassword = () => {
+interface IProps {
+    setLoading: Dispatch<SetStateAction<boolean>>;
+}
+
+export const AuthResetPassword: FC<IProps> = ({ setLoading }): JSX.Element => {
     const emailRef = useRef<HTMLInputElement>(null);
 
     const navigate = useNavigate();
 
     const handleResetPassword = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setLoading(true);
     
         sendPasswordResetEmail(auth, emailRef.current!.value)
             .then(() => {
                 toast.success("Check your email for a reset link");
                 navigate(Routes.LOGIN);
+                setLoading(false);
             })
             .catch((error) => {
                 toast.error(error.message);
+                setLoading(false);
             });
     };
 
--- src/pages/ResetPassword/ResetPassword.tsx
+++ src/pages/ResetPassword/ResetPassword.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState, FC } from 'react'
 
-import { AuthResetPassword } from '../../components';
+import { AuthResetPassword, Spinner } from '../../components';
 
 import logo from "../../assets/logo.png";
 import wallpaper from "../../assets/wallpaper.png";
 
-const ResetPassword = () => {
+const ResetPassword: FC = (): JSX.Element => {
+    const [ loading, setLoading ] = useState<boolean>(false);
+
     return (
         <div className="flex items-center">
 			<div className="relative w-1/2">
@@ -13,8 +15,11 @@ const ResetPassword = () => {
                 <img className="w-full h-screen relative" src={wallpaper} alt="wallpaper.png"/>
             </div>
 			<div className="w-1/2">
-                <AuthResetPassword/>
+                <AuthResetPassword
+                    setLoading={setLoading}
+                />
 			</div>
+            { loading && <Spinner/> }
 		</div>
     )
 }
