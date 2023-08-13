897721b52990f686c15f6ff9100bdba4c9380ada
stefan022 <stefan022github@gmail.com>
2023-08-13 08:30:06 +0200

    refactor the last commit

--- src/features/slices/phonesSlice.ts
+++ src/features/slices/phonesSlice.ts
@@ -4,30 +4,19 @@ import { IPhone } from "../../ts/interfaces/IProducts/IPhone";
 
 interface InitialState {
 	phones: IPhone[];
-	singlePhone: IPhone | null;
 };
 
 const initialState: InitialState = {
 	phones: [],
-	singlePhone: null,
 };
 
 export const phonesSlice = createSlice({
 	name: "phones",
 	initialState,
-	reducers: {
-		RESET_SINGLE_PHONE: (state) => {
-			state.singlePhone = null;
-		}
-	},
+	reducers: {},
 	extraReducers: (builder) => {
 		builder.addMatcher(phonesAPI.endpoints.getPhones.matchFulfilled, (state, action: PayloadAction<IPhone[]>) => {
 			state.phones = action.payload;
 		});
-		builder.addMatcher(phonesAPI.endpoints.getSinglePhone.matchFulfilled, (state, action: PayloadAction<IPhone>) => {
-			state.singlePhone = action.payload;
-		});
 	},
 });
-
-export const { RESET_SINGLE_PHONE } = phonesSlice.actions;
--- src/pages/Products/Phones/SinglePhone/SinglePhone.tsx
+++ src/pages/Products/Phones/SinglePhone/SinglePhone.tsx
@@ -3,28 +3,12 @@ import React, { FC } from 'react'
 import { PhoneDetails, Spinner } from "../../../../components"
 import { useParams } from 'react-router-dom';
 import { useGetSinglePhoneQuery } from '../../../../features/API/phonesAPI';
-import { useAppSelector } from '../../../../hooks/useAppSelector';
-import { RootState } from '../../../../ts/types/RootState';
-import { useAppDispatch } from '../../../../hooks/useAppDispatch';
-import { RESET_SINGLE_PHONE } from '../../../../features/slices/phonesSlice';
-
 
 const SinglePhone: FC = (): JSX.Element | null => {
-    const { singlePhone } = useAppSelector((state: RootState) => state.phones);
     const { productId } = useParams();
 
-    useGetSinglePhoneQuery(Number(productId!));
+    const { data: singlePhone } = useGetSinglePhoneQuery(+productId!);
     
-    const dispatch = useAppDispatch();
-    
-    React.useEffect(() => {
-        return () => {
-            dispatch(RESET_SINGLE_PHONE());
-        }
-        // eslint-disable-next-line
-    }, [productId]);
-
-
     if (singlePhone) {
         return <PhoneDetails product={singlePhone}/>
     }
