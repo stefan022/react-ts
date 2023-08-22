63f4e34bfde4d6d39faf7bcb58a0dd2fc0676de9
stefan022 <stefan022github@gmail.com>
2023-08-22 22:09:18 +0200

    add DarkThemeContext in ProductRatingsProgress component

--- src/components/Products/ProductDetails/ProductRatings/ProductRatingsProgress/ProductRatingsProgress.tsx
+++ src/components/Products/ProductDetails/ProductRatings/ProductRatingsProgress/ProductRatingsProgress.tsx
@@ -1,13 +1,17 @@
-import React, { FC } from "react";
+import React, { FC, useContext, Context } from "react";
 
 import { AiFillStar } from "react-icons/ai";
 import { stars } from "../../../../../constants/stars";
+import DarkThemeContext from "../../../../../context/ThemeContext";
+import { IDarkThemeContext } from "../../../../../ts/interfaces/IDarkThemeContext/IDarkThemeContext";
 
 interface IProps {
 	ratingReviews: number;
 }
 
 const ProductRatingsProgress: FC<IProps> = ({ ratingReviews }): JSX.Element => {
+	const { darkTheme } = useContext(DarkThemeContext as Context<IDarkThemeContext>);
+
 	return (
 		<div className="flex flex-col-reverse p-6 w-4/12">
 			{
@@ -20,11 +24,15 @@ const ProductRatingsProgress: FC<IProps> = ({ ratingReviews }): JSX.Element => {
 								<AiFillStar size={24} fill="#ffcc00" />
 								<p>{starRating}</p>
 							</div>
-							<div className="w-full bg-gray-200 rounded-full h-2.5">
+							<div className={`
+								${ darkTheme ? "bg-gray-700" : "bg-gray-200" }
+								w-full rounded-full h-2.5
+							`}>
 								<div
 									className={`
 										${starRating === ratingReviews ? "w-full" : "w-0"}
-										bg-blue-400 h-2.5 rounded-full
+										${ darkTheme ? "bg-blue-600" : "bg-blue-400" }
+										h-2.5 rounded-full
 									`}
 								></div>
 							</div>
