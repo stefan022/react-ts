fcc4ad5da902d9ce091a9bba9ab69c9990b859aa
stefan022 <stefan022github@gmail.com>
2023-08-22 17:28:23 +0200

    add DarkThemeContext in Banner component

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,7 +1,9 @@
-import React, { FC} from 'react'
+import React, { FC, useContext, Context } from 'react'
 
 import { BannerNavigation } from "../../components"
 import { IBannerNavigation } from '../../ts/interfaces/IBanner/IBannerNavigation';
+import DarkThemeContext from '../../context/ThemeContext';
+import { IDarkThemeContext } from '../../ts/interfaces/IDarkThemeContext/IDarkThemeContext';
 
 interface IProps {
     title: string;
@@ -13,14 +15,18 @@ interface IProps {
 }
 
 const Banner: FC<IProps> = ({ title, desc, navigation, bgPosition, bgNoRepeat, image }): JSX.Element => {
+    const { darkTheme } = useContext(DarkThemeContext as Context<IDarkThemeContext>);
+
+    const lightBgImage = `linear-gradient(90deg, rgba(45,139,255,.9) 40%, rgba(96,165,250,.9) 80%), url(${image})`;
+    const darkBgImage = `linear-gradient(90deg, rgba(29,78,216,.9) 0%, rgba(37,99,235,.9) 100%), url(${image})`;
+
+    const bgImage = darkTheme ? darkBgImage : lightBgImage;
+
     return (
         <div
-            style={{ 
-                backgroundImage: 
-                `linear-gradient(90deg, rgba(45,139,255,.9) 40%, rgba(96,165,250,.9) 80%), 
-                url(${image})` 
-            }} 
-                className={`${bgPosition} ${bgNoRepeat} h-[200px] w-full flex flex-col items-center justify-center text-white`}>
+            style={{ backgroundImage: bgImage }} 
+            className={`${bgPosition} ${bgNoRepeat} h-[200px] w-full flex flex-col items-center justify-center text-white`}
+        >
             <h1 className="text-white font-normal">{title}</h1>
             {
                 navigation
