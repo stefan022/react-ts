ad095872f87057cedf88037deb1ff74c896831c6
stefan022 <stefan022github@gmail.com>
2023-08-23 03:56:21 +0200

    add DarkThemeContext in WishlistEmpty component

--- src/components/Profile/Wishlist/WishlistEmpty/WishlistEmpty.tsx
+++ src/components/Profile/Wishlist/WishlistEmpty/WishlistEmpty.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { FC, useContext, Context } from 'react'
 
 import empty_wishlist from "../../../../assets/empty-wishlist.png";
 import { Link } from 'react-router-dom';
 import { Routes } from '../../../../router/Routes';
+import DarkThemeContext from '../../../../context/ThemeContext';
+import { IDarkThemeContext } from '../../../../ts/interfaces/IDarkThemeContext/IDarkThemeContext';
 
-const WishlistEmpty = () => {
+const WishlistEmpty: FC = (): JSX.Element => {
+    const { darkTheme } = useContext(DarkThemeContext as Context<IDarkThemeContext>);
+
     return (
         <div className='p-4 flex flex-col gap-6 justify-center items-center border border-gray-300 w-full h-[350px]'>
             <div className='flex flex-col gap-2 justify-center items-center'>
@@ -12,7 +16,11 @@ const WishlistEmpty = () => {
                 <h2>Your wishlist is empty.</h2>
             </div>
             <Link to={Routes.PRODUCTS}>
-                <button className='py-1 px-2 bg-blue-400 hover:bg-blue-500 text-white rounded-md'>Start Shopping</button>
+                <button 
+                    className={`
+                        ${ darkTheme ? "bg-blue-600 hover:bg-blue-700" : "bg-blue-400 hover:bg-blue-500" }
+                        py-1 px-2  text-white rounded-md
+                    `}>Start Shopping</button>
             </Link>
         </div>
     )
