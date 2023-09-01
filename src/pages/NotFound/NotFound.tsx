e4c674a16966d196022cb31660f734e9fa9a5a45
stefan022 <stefan022github@gmail.com>
2023-09-01 02:43:47 +0200

    refactor NotFound page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { Context, FC, useContext } from 'react'
 
 import error404 from "../../assets/error404.png";
 import { Link } from 'react-router-dom';
 import { Routes } from '../../router/Routes';
+import DarkThemeContext from '../../context/ThemeContext';
+import { IDarkThemeContext } from '../../ts/interfaces/IDarkThemeContext/IDarkThemeContext';
 
-const NotFound = () => {
+import "./NotFound.scss"
+
+const NotFound: FC = (): JSX.Element => {
+    const { darkTheme } = useContext(DarkThemeContext as Context<IDarkThemeContext>);
+
+    const notFound = darkTheme ? "not-found-dark" : "not-found";
+    const notFoundButton = darkTheme ? "not-found__button-dark" : "not-found__button";
+
     return (
-        <div className='flex flex-col justify-center items-center h-screen'>
+        <div className={notFound}>
             <img width={300} src={error404} alt="error404.png"/>
-            <h2 className='mt-10 text-lg font-bold'>Sorry, page not found!</h2>
+            <h2 className='not-found__message'>
+                Sorry, page not found!
+            </h2>
             <Link to={Routes.HOME}>
-                <button className='bg-yellow-300 hover:bg-yellow-400 transition-all py-1 px-3 mt-2 rounded-md'>Back to Home page</button>
+                <button className={notFoundButton}>
+                    Back to Home page
+                </button>
             </Link>
         </div>
     )
