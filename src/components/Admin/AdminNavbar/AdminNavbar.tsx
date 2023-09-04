aaad0f3754badd48c73f12bad3b6a3befe9b0698
stefan022 <stefan022github@gmail.com>
2023-09-04 14:29:41 +0200

    add AdminNavbar.scss

--- src/components/Admin/AdminNavbar/AdminNavbar.tsx
+++ src/components/Admin/AdminNavbar/AdminNavbar.tsx
@@ -3,6 +3,8 @@ import React, { FC } from 'react'
 import { IoMdNotificationsOutline } from 'react-icons/io'
 import { ThemeToggleSwitch, NavbarProfilePicture } from '../../../components'
 
+import "./AdminNavbar.scss"
+
 interface IProps {
     darkTheme: boolean;
     setDarkTheme: (arg: boolean) => void;
@@ -12,15 +14,12 @@ const AdminNavbar: FC<IProps> = ({ darkTheme, setDarkTheme }): JSX.Element => {
     const handleChangeTheme = () => setDarkTheme(!darkTheme);
 
     return (
-        <div className={`
-            ${ darkTheme ? "bg-gray-900 text-gray-300" : "bg-white text-gray-700" }
-            py-5 px-4 flex justify-end items-center gap-2 border border-gray-400
-        `}>
+        <div className={ darkTheme ? "admin__navbar-dark" : "admin__navbar" }>
             <ThemeToggleSwitch
                 darkTheme={darkTheme}
                 handleChangeTheme={handleChangeTheme}
             />
-            <div className='w-[40px] h-[40px] rounded-full flex items-center justify-center cursor-pointer hover:bg-gray-100 relative -left-1'>
+            <div className={ darkTheme ? "admin__navbar_notifications-dark" : "admin__navbar_notifications" }>
                 <IoMdNotificationsOutline color='gray' size={20}/>
             </div>
             <NavbarProfilePicture/>
